7e767c8898fc92a1e9acdfb2bdaab871b1ab5b16
Sanchit <sanchitgangwar457@gmail.com>
2026-03-16 14:45:03 +0530

    third commit

--- 2dmatrix.py
+++ 2dmatrix.py
@@ -1,4 +1,4 @@
-nums=[[1,2,3],[4,5,6],[7,8,9],[10,11,12,],[13,14,15]]
+# nums=[[1,2,3],[4,5,6],[7,8,9],[10,11,12,],[13,14,15]]
 # def twod_matrix(nums):
 #     for i in range(0,len(nums)):
 #         for j in range(0,len(nums[0])):
@@ -30,13 +30,22 @@ nums=[[1,2,3],[4,5,6],[7,8,9],[10,11,12,],[13,14,15]]
 #                 print(nums[i][j],end=" ")
 # diagonal(nums)
 
-def downward_diagonal(nums):
-    row=0
-    col=len(nums[0])-1
-    while row<len(nums):
-        while col>=0:
-            print(nums[row][col])
-            col-=1
-            row+=1
-downward_diagonal(nums)
+# def downward_diagonal(nums):
+#     row=0
+#     col=len(nums[0])-1
+#     while row<len(nums):
+#         while col>=0:
+#             print(nums[row][col])
+#             col-=1
+#             row+=1
+# downward_diagonal(nums)
 
+# transpose of 2d matrix 
+nums=[[5,9,1],[2,3,7]]
+row = len(nums)
+col= len(nums[0])
+result=[[0]*row for _ in range(col)]
+for i in range(0,row):
+    for j in range(0,col):
+        result[j][i]=nums[i][j]
+print(result)
