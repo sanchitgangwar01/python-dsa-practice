fa55b302aa6f7656213cb8e035b419b8f33b5c59
Sanchit <sanchitgangwar457@gmail.com>
2026-03-20 14:43:17 +0530

    Recursion Method in binary search

--- 46_binary_search.py
+++ 46_binary_search.py
@@ -1,17 +1,34 @@
-nums=[2,3,4,5,7,9]
-tar=0
-def binarySearch(nums,tar):
-    n=len(nums)
-    low=0
-    high=n-1
-    while low<high:
-        mid=(low+high)//2
-        if nums[mid]==tar:
-            return tar
-        elif tar<nums[mid]:
-            high=mid-1
-        else:
-            low=mid+1
-    return -1
+# nums=[2,3,4,5,7,9]
+# tar=0
+# def binarySearch(nums,tar):
+#     n=len(nums)
+#     low=0
+#     high=n-1
+#     while low<high:
+#         mid=(low+high)//2
+#         if nums[mid]==tar:
+#             return tar
+#         elif tar<nums[mid]:
+#             high=mid-1
+#         else:
+#             low=mid+1
+#     return -1
 
-print(binarySearch(nums,tar))
+# print(binarySearch(nums,tar))
+
+
+# Recursion Method 
+nums= [2,3,4,5,7,9]
+target=7
+def binarySerch(nums,low,high):
+    if low>high:
+        return -1
+    mid=(low+high)//2
+    if nums[mid]==target:
+        return mid
+    elif nums[mid]<target:
+        return binarySerch(nums,mid+1,high)
+    else:
+       return binarySerch(nums,low,mid-1)
+
+print(binarySerch(nums,0,5))
